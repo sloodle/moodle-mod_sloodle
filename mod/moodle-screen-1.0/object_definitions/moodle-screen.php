0e17886add36e5d0d389543f7fa14a013818d6bd
Edmund Edgar <ed@socialminds.jp>
2011-06-30 23:32:52 +0000

    Checked in Moodle screen object definition

---
+++ mod/moodle-screen-1.0/object_definitions/moodle-screen.php
@@ -0,0 +1,10 @@
+<?php
+$sloodleconfig = new SloodleObjectConfig();
+$sloodleconfig->primname   = 'SLOODLE Moodle Screen';
+$sloodleconfig->object_code= 'moodle-screen';
+$sloodleconfig->group      = 'misc';
+$sloodleconfig->show       = true;
+$sloodleconfig->aliases    = array('SLOODLE 1.1 Moodle Screen');
+$sloodleconfig->field_sets = array( 
+);
+?>
