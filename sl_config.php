662ba849acc3bd7d904358a15f8d94e06015c9eb
Edmund Edgar <ed@socialminds.jp>
2011-07-30 06:08:37 +0000

    commented-out defines for login/logout options

--- sl_config.php
+++ sl_config.php
@@ -37,6 +37,7 @@
     /** The Sloodle version number. */
     define('SLOODLE_VERSION', 2.0); // This is the release version, not the module version (which is in version.php)
 
+    // The following tells us whether Moodle is at > version 2  or not. 
     define('SLOODLE_IS_ENVIRONMENT_MOODLE_2', ($CFG->version >= 2010060800) );
 
     
@@ -141,11 +142,35 @@
     */
     define('SLOODLE_DEBUG_REQUEST_LOG', '/tmp/sloodle_debug.log');
 
-    // TODO: Implement this:
+    // This hasn't yet been implemented.
     /** The following tells objects that we want them to persist their config over resets, and copy it to new objects that are copied.
     * This should usually be on, but if you're developing a set to share with other people, it's better to turn it off
     * That way the objects you rez won't have your server details in them.
+    * 
     */
     define('SLOODLE_ENABLE_OBJECT_PERSISTANCE', true);
+
+//---------------------------------------------------------------------
+   
+    // Login and top-level navigation customization
+    /** 
+    * By default, the shared-media version of the set (and potentially other tools) will use the regular Moodle pages to login or logout the user.
+    * This isn't ideal UI-wise, because those screens are designed to be displayed in a browser, not on a shared-media prim.
+    * By setting an include here, you can supply your own login screen code.
+    * This was designed for Avatar Classroom, where we want to redirect you to our shared login screen at avatarclassroom.com
+    * ...but it may also be useful if you want to make a shared-media-specific login screen
+    * ...or you have an unusual login flow that requires customization.
+    */
+    //define('SLOODLE_SHARED_MEDIA_LOGIN_INCLUDE', SLOODLE_DIRROOT.'/mod/set-1.0/shared_media/login.avatarclassroom.php');
+    //define('SLOODLE_SHARED_MEDIA_LOGOUT_INCLUDE', SLOODLE_DIRROOT.'/mod/set-1.0/shared_media/logout.avatarclassroom.php');
+
+    // Site list customization
+    /**
+    * This allows you to have a back button on the shared media screen 
+    * Used in Avatar Classroom to provide a list of your sites so that you can switch between them or create a new one.
+    * Probably not useful to anyone else.
+    */
+    //define('SLOODLE_SHARED_MEDIA_SITE_LIST_BASE_URL', 'http://api.avatarclassroom.com/mod/sloodle/mod/set-1.0/shared_media/');
+
 
 ?>
