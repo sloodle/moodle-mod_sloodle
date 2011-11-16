9c7d5fd324feb1e7de89c9c9c961c97d5331f18b
Edmund Edgar <ed@socialminds.jp>
2011-11-16 04:04:30 +0000

    Added missing toolbar giver definition. (Object should already be in the set.)

---
+++ mod/toolbar_giver-1.0/object_definitions/default.php
@@ -0,0 +1,11 @@
+<?php
+$sloodleconfig = new SloodleObjectConfig();
+$sloodleconfig->primname   = 'SLOODLE Toolbar Giver';
+$sloodleconfig->object_code= 'default';
+$sloodleconfig->modname    = 'toolbar_giver-1.0';
+$sloodleconfig->group      = 'communication';
+$sloodleconfig->show       = true;
+$sloodleconfig->aliases    = array('SLOODLE 1.1 Toolbar Giver', 'SLOODLE 1.2 Toolbar Giver');
+$sloodleconfig->field_sets = array( 
+);
+?>
