cd7d467a4b691bff1f7d46cb48883f01fc34f965
Edmund Edgar <ed@socialminds.jp>
2012-04-25 09:42:08 +0000

    Checked in Avatar Classroom building definitions. (Turned off by sl_config for regular SLOODLE).

---
+++ mod/building-1.0/object_definitions/AvatarClassroomFloors234.php
@@ -0,0 +1,8 @@
+<?php
+$sloodleconfig = new SloodleObjectConfig();
+$sloodleconfig->primname   = 'AvatarClassroomFloors234';
+$sloodleconfig->object_code= 'AvatarClassroomFloors234';
+$sloodleconfig->modname    = 'building-1.0';
+$sloodleconfig->group      = 'misc';
+$sloodleconfig->collections= array('Avatar Classroom 2.0 Furniture A');
+//parameter name, translation text, description, default value, length
---
+++ mod/building-1.0/object_definitions/SharkPool.php
@@ -0,0 +1,10 @@
+<?php
+$sloodleconfig = new SloodleObjectConfig();
+$sloodleconfig->primname   = 'SharkPool';
+$sloodleconfig->object_code= 'SharkPool';
+$sloodleconfig->modname    = 'building-1.0';
+$sloodleconfig->group      = 'misc';
+$sloodleconfig->collections= array('Avatar Classroom 2.0 Furniture A');
+//parameter name, translation text, description, default value, length
+
+?>
