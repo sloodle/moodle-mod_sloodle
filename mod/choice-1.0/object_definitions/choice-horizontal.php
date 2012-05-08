43777819d36bf26abec447212078dcc8469c6b75
Edmund Edgar <ed@socialminds.jp>
2012-05-08 22:43:25 +0000

    Removed horizontal choice from the standard menu, standardized on the vertical one. If there's a use-case for the horizontal method we should probably code the choice to flip when rotated.

---
+++ mod/choice-1.0/object_definitions/choice-horizontal.php
@@ -0,0 +1,22 @@
+<?php
+$sloodleconfig = new SloodleObjectConfig();
+$sloodleconfig->primname   = 'SLOODLE Choice (Horizontal)';
+$sloodleconfig->object_code= 'choice-horizontal';
+$sloodleconfig->modname    = 'choice-1.0';
+$sloodleconfig->module     = 'choice';
+$sloodleconfig->module_choice_message = 'selectchoice';
+$sloodleconfig->module_no_choices_message= 'nochoices';
+$sloodleconfig->group      = 'communication';
+$sloodleconfig->collections= array('SLOODLE 1.0'); // No longer displayed in SLOODLE 2.0 - settled on a single vertical one
+$sloodleconfig->aliases    = array('SLOODLE 1.1 Choice (Horizontal)');
+$sloodleconfig->field_sets = array( 
+	'generalconfiguration' => array(
+		'sloodlerefreshtime' => new SloodleConfigurationOptionText( 'sloodlerefreshtime', 'refreshtimeseconds', '', 600, 8 ),
+		'sloodlerelative' => new SloodleConfigurationOptionYesNo( 'sloodlerelative', 'relativeresults', ''  ),
+	),
+	'accesslevel' => array(
+		'sloodleobjectaccessleveluse'  => $sloodleconfig->access_level_object_use_option(),
+		'sloodleserveraccesslevel'     => $sloodleconfig->access_level_server_option(),
+	)
+);
+?>
--- mod/choice-1.0/object_definitions/default.php
+++ mod/choice-1.0/object_definitions/default.php
@@ -1,6 +1,6 @@
 <?php
 $sloodleconfig = new SloodleObjectConfig();
-$sloodleconfig->primname   = 'SLOODLE Choice (Horizontal)';
+$sloodleconfig->primname   = 'SLOODLE Choice';
 $sloodleconfig->object_code= 'default';
 $sloodleconfig->modname    = 'choice-1.0';
 $sloodleconfig->module     = 'choice';
@@ -8,15 +8,14 @@ $sloodleconfig->module_choice_message = 'selectchoice';
 $sloodleconfig->module_no_choices_message= 'nochoices';
 $sloodleconfig->group      = 'communication';
 $sloodleconfig->collections= array('SLOODLE 2.0');
-$sloodleconfig->aliases    = array('SLOODLE 1.1 Choice (Horizontal)');
+$sloodleconfig->aliases    = array('SLOODLE 1.1 Choice (Vertical)', 'SLOODLE 1.1 Choice (Vertical)');
 $sloodleconfig->field_sets = array( 
-	'generalconfiguration' => array(
-		'sloodlerefreshtime' => new SloodleConfigurationOptionText( 'sloodlerefreshtime', 'refreshtimeseconds', '', 600, 8 ),
-		'sloodlerelative' => new SloodleConfigurationOptionYesNo( 'sloodlerelative', 'relativeresults', ''  ),
-	),
-	'accesslevel' => array(
+	'access' => array(
 		'sloodleobjectaccessleveluse'  => $sloodleconfig->access_level_object_use_option(),
 		'sloodleserveraccesslevel'     => $sloodleconfig->access_level_server_option(),
-	)
+	),
+	'generalconfiguration' => array(
+		'sloodlerefreshtime' => new SloodleConfigurationOptionText( 'sloodlerefreshtime', 'refreshtimeseconds', '', 600, 8 ),
+	),
 );
 ?>
