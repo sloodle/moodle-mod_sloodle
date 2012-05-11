1206f027c3f1c7e5850ce83fc2b3df4fd175a45a
Edmund Edgar <ed@socialminds.jp>
2012-05-11 05:14:24 +0000

    Added a comment explaining that some stuff may go in sloodle_config.php as well as init.php

--- sl_config.php
+++ sl_config.php
@@ -1,6 +1,8 @@
 <?php
 /*
-This file has been replaced by init.php.
+This file has been replaced by the following files: 
+ init.php - core settings that should be changed on upgrades.
+ sloodle_config.php - customized settings that should survive upgrades.
 Calling it is deprecated, but it has been left for backwards compatibility to ease upgrades.
 */
 require_once(realpath(dirname(__FILE__) . "/" . "init.php"));
