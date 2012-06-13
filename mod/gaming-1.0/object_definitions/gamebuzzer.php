cb7adfcda267f43279e00ca57bdaeacba9db25bf
Paul Preibisch <fire@b3dmultitech.com>
2012-06-13 17:01:19 +0900

    changed the collection to: $sloodleconfig->collections= array('Avatar Classroom 2.0 Gaming A');

--- mod/gaming-1.0/object_definitions/gamebuzzer.php
+++ mod/gaming-1.0/object_definitions/gamebuzzer.php
@@ -2,10 +2,10 @@
 $sloodleconfig = new SloodleObjectConfig();
 $sloodleconfig->primname   = 'Game Buzzer';
 $sloodleconfig->object_code= 'GameBuzzer';
-$sloodleconfig->modname    = 'furniture-1.0';
+$sloodleconfig->modname    = 'gaming-1.0';
 $sloodleconfig->group      = 'misc';
 $sloodleconfig->aliases    = array('GameBuzzer');
-$sloodleconfig->collections= array('Avatar Classroom 2.0 Furniture A');
+$sloodleconfig->collections= array('Avatar Classroom 2.0 Gaming A');
 //parameter name, translation text, description, default value, length
 $sloodleconfig->field_sets = array(
 'generalconfiguration'=> array(
