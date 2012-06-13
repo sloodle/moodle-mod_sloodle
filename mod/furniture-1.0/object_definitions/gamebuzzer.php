68a7be50fb34418b9051f9e7b6decbc3b76c6740
Paul Preibisch <fire@b3dmultitech.com>
2012-06-13 16:40:01 +0900

    removed facilitators from config

--- mod/furniture-1.0/object_definitions/gamebuzzer.php
+++ mod/furniture-1.0/object_definitions/gamebuzzer.php
@@ -9,10 +9,5 @@ $sloodleconfig->collections= array('Avatar Classroom 2.0 Furniture A');
 //parameter name, translation text, description, default value, length
 $sloodleconfig->field_sets = array(
 'generalconfiguration'=> array(
- 'time' => new SloodleConfigurationOptionText( 'time', 'misc:gamebuzzer_time', '', '00:00:00', 15 ),
- 'facilitator' => new SloodleConfigurationOptionText( 'facilitator', 'misc:facilitator', '', '', 15 ),
- 'facilitator' => new SloodleConfigurationOptionText( 'facilitator', 'misc:facilitator', '', '', 15 ), 
- 'facilitator' => new SloodleConfigurationOptionText( 'facilitator', 'misc:facilitator', '', '', 15 ),
- 'facilitator' => new SloodleConfigurationOptionText( 'facilitator', 'misc:facilitator', '', '', 15 )
- 
+ 'time' => new SloodleConfigurationOptionText( 'time', 'misc:gamebuzzer_time', '', '00:00:00', 15 )
  ));
