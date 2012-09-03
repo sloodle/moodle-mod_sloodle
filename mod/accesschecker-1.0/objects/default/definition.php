18f339164b4a976405695b1143f12a6fcc647346
Edmund Edgar <ed@socialminds.jp>
2012-09-03 23:51:34 +0000

    Removed access checker from rezzer screen

--- mod/accesschecker-1.0/objects/default/definition.php
+++ mod/accesschecker-1.0/objects/default/definition.php
@@ -2,7 +2,7 @@
 $sloodleconfig = new SloodleObjectConfig();
 $sloodleconfig->primname   = 'SLOODLE Access Checker';
 $sloodleconfig->group      = 'registration';
-$sloodleconfig->collections= array('SLOODLE 2.0');
+$sloodleconfig->collections= array('SLOODLE 1.0');
 $sloodleconfig->aliases    = array('SLOODLE 1.1 Access Checker');
 $sloodleconfig->field_sets = array( 
 	'accesslevel' => array(
