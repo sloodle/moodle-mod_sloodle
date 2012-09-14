c44cd121428fecf812aa857808767d305da3f241
Edmund Edgar <ed@socialminds.jp>
2012-09-14 01:12:48 +0000

    Renamed legacy presenter definition, which was causing the new presenter go missing.

--- mod/presenter-1.0/objects/default/definition.php
+++ mod/presenter-1.0/objects/default/definition.php
@@ -1,13 +1,13 @@
 <?php
 $sloodleconfig = new SloodleObjectConfig();
-$sloodleconfig->primname   = 'SLOODLE Presenter';
+$sloodleconfig->primname   = 'SLOODLE Presenter (Parcel Media)';
 $sloodleconfig->module     = 'sloodle';
 $sloodleconfig->module_choice_message = 'selectpresenter';
 $sloodleconfig->module_no_choices_message = 'nopresenters'; 
 $sloodleconfig->module_filters = array( 'type' => SLOODLE_TYPE_PRESENTER); 
 $sloodleconfig->group      = 'communication';
 $sloodleconfig->collections= array('SLOODLE 1.0'); // Deprecated as of SLOODLE 2.0
-$sloodleconfig->aliases    = array('SLOODLE 1.1 Presenter');
+$sloodleconfig->aliases    = array('SLOODLE Presenter', 'SLOODLE 1.1 Presenter');
 $sloodleconfig->field_sets = array( 
 	'accesslevel' => array(
 		'sloodleobjectaccessleveluse'   => $sloodleconfig->access_level_object_use_option(),
